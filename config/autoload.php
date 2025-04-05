76192eb8b1b07216c69424800018c80673ddcd7a
Nathan Chevrollier <nathan.chevrollier17@orange.fr>
2025-04-05 03:59:50 +0200

    test vps

--- config/autoload.php
+++ config/autoload.php
@@ -5,16 +5,22 @@ require_once __DIR__ . '/constants.php';
 // Fonction d'autoload pour charger automatiquement les classes
 
 spl_autoload_register(function ($className) {
-    // Convertir les espaces de noms en chemins de fichiers
+    // Remplace les \ par des /
     $className = str_replace('\\', DIRECTORY_SEPARATOR, $className);
-    $filePath = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . $className . '.php';
-    
-    // Vérifier si le fichier existe
+
+    // Si premier dossier est en minuscule, on le met en maj
+    $segments = explode(DIRECTORY_SEPARATOR, $className);
+    if (isset($segments[0])) {
+        $segments[0] = ucfirst($segments[0]);
+    }
+
+    $filePath = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $segments) . '.php';
+
     if (file_exists($filePath)) {
         require_once $filePath;
         return true;
     }
-    
+
     return false;
 });
 
